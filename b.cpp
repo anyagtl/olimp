763f44bbe674a097990fc21161a6591e8dfb92f8
anyagtl <kseniya.lipskaya06@gmail.com>
2024-04-07 17:54:24 +0300

    commit at 2024-04-07 17:54:24.743664

--- b.cpp
+++ b.cpp
@@ -1,48 +1,5 @@
 #include <bits/extc++.h>
-
-#define Int long long
-//#define FAST_ALLOCATOR_MEMORY 2e8
-#define fast_io
-
-#ifdef FAST_ALLOCATOR_MEMORY
-    int allocator_pos = 0;
-    char allocator_memory[(int)FAST_ALLOCATOR_MEMORY];
-    inline void * operator new ( size_t n ) {
-        char *res = allocator_memory + allocator_pos;
-        allocator_pos += n;
-        assert(allocator_pos <= (int)FAST_ALLOCATOR_MEMORY);
-        return (void *)res;
-    }
-    inline void operator delete ( void * ) noexcept { }
-    inline void operator delete ( void *, size_t ) noexcept { }
-    //inline void * operator new [] ( size_t ) { assert(0); }
-    //inline void operator delete [] ( void * ) { assert(0); }
-#endif
-
-
-template <class T = int> inline T readInt();
-inline double readDouble();
-inline int readUInt();
-inline int readChar();
-inline void readWord( char *s );
-inline bool readLine( char *s );
-inline bool isEof();
-inline int getChar(); 
-inline int peekChar();
-inline bool seekEof();
-inline void skipBlanks();
-
-template <class T> inline void writeInt( T x, char end = 0, int len = -1 );
-inline void writeChar( int x );
-inline void writeWord( const char *s );
-inline void writeDouble( double x, int len = 0 );
-inline void flush();
-
-static struct buffer_flusher_t {
-    ~buffer_flusher_t() {
-        flush();
-    }
-} buffer_flusher;
+#define int long long
 
 struct node {
     int val;
@@ -61,6 +18,11 @@ struct node {
         *this = node();
         val = _val;
     }
+};
+
+struct n2 {
+    int first, second;
+    n2() {}
 };
 
 struct segtree {
@@ -171,47 +133,15 @@ struct segtree {
 };
 
 
-
-std::mt19937_64 gen(std::chrono::steady_clock::now().time_since_epoch().count());
-const int INF = std::numeric_limits<int>::max();
-
-inline Int randint(Int le, Int ri) {
-    return le + (gen() % (ri - le + 1));
-}
-
-template<typename T1, typename T2>
-inline void mineq(T1 &a, const T2 &b) {
-    if (a > b) {
-        a = b;
-    }
-}
-
-template<typename T1, typename T2>
-inline void maxeq(T1 &a, const T2 &b) {
-    if (a < b) {
-        a = b;
-    }
-}
-
-template<typename T>
-inline T sig(T a) {
-    return (a == 0 ? 0 : (a > 0 ? 1 : -1));
-}
-
-template<typename T>
-inline T _abs(T x) {
-    return x >= 0 ? x : -x;
-}
-
-
 signed main() {
     int n;
-    n = readInt();
-    std::vector<std::pair<int, int>> v(n);
+    std::cin >> n;
+    std::vector<n2> v(n);
     for (int i = 0; i < n; i++) {
-        v[i].first = readInt();
+        std::cin >> v[i].first;
         v[i].second = i;
     }
+
 
     std::vector<node> l(n);
     std::vector<node> r(n);
@@ -224,7 +154,7 @@ signed main() {
     segtree sgl = segtree(n, l);
     segtree sgr = segtree(n, r);
 
-    std::sort(v.begin(), v.end(), [](std::pair<int, int> a, std::pair<int, int> b) { return a.first < b.first; });
+    std::sort(v.begin(), v.end(), [](n2 a, n2 b) { return a.first < b.first; });
     int ans = 0;
 
     for (int i = 0; i < n; i++) {
@@ -238,214 +168,6 @@ signed main() {
         sgl.eq_upd(v[i].second, v[i].second + 1, node(0), 0, n, 0);
     }
 
-    writeInt(ans);
-    writeChar('\n');
+    std::cout << ans << '\n';
     return 0;
 }
-
-static const int buf_size = 4096;
-
-static unsigned char buf[buf_size];
-static int buf_len = 0, buf_pos = 0;
-
-inline bool isEof() {
-    if (buf_pos == buf_len) {
-        buf_pos = 0;
-        buf_len = fread(buf, 1, buf_size, stdin);
-        if (buf_pos == buf_len) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-inline int getChar() {
-    return isEof() ? -1 : buf[buf_pos++];
-}
-
-inline int peekChar() {
-    return isEof() ? -1 : buf[buf_pos];
-}
-
-inline bool seekEof() {
-    int c;
-    while ((c = peekChar()) != -1 && c <= 32) {
-        buf_pos++;
-    }
-    return c == -1;
-}
-
-inline void skipBlanks() {
-    while (!isEof() && buf[buf_pos] <= 32U) {
-        buf_pos++;
-    }
-}
-
-inline int readChar() {
-    int c = getChar();
-    while (c != -1 && c <= 32) {
-        c = getChar();
-    }
-    return c;
-}
-
-inline int readUInt() {
-    int c = readChar(), x = 0;
-    while ('0' <= c && c <= '9') {
-        x = x * 10 + c - '0';
-        c = getChar();
-    }
-    return x;
-}
-
-template <class T>
-inline T readInt() {
-    int s = 1, c = readChar();
-    T x = 0;
-    if (c == '-') {
-        s = -1, c = getChar();
-    } else if (c == '+') {
-        c = getChar();
-    }
-    while ('0' <= c && c <= '9') {
-        x = x * 10 + c - '0';
-        c = getChar();
-    }
-    return s == 1 ? x : -x;
-}
-
-inline double readDouble() {
-    int s = 1, c = readChar();
-    double x = 0;
-    if (c == '-') {
-        s = -1;
-        c = getChar();
-    }
-    while ('0' <= c && c <= '9') {
-        x = x * 10 + c - '0';
-        c = getChar();
-    }
-    if (c == '.') {
-        c = getChar();
-        double coef = 1;
-        while ('0' <= c && c <= '9') {
-            x += (c - '0') * (coef *= 1e-1);
-            c = getChar();
-        }
-    }
-    return s == 1 ? x : -x;
-}
-
-inline void readWord( char *s ) {
-    int c = readChar();
-    while (c > 32) {
-        *s++ = c;
-        c = getChar();
-    }
-    *s = 0;
-}
-
-inline bool readLine( char *s ) {
-    int c = getChar();
-    while (c != '\n' && c != -1) {
-        *s++ = c;
-        c = getChar();
-    }
-    *s = 0;
-    return c != -1;
-}
-
-
-static int write_buf_pos = 0;
-static char write_buf[buf_size];
-
-inline void writeChar( int x ) {
-    if (write_buf_pos == buf_size) {
-        fwrite(write_buf, 1, buf_size, stdout);
-        write_buf_pos = 0;
-    }
-    write_buf[write_buf_pos++] = x;
-}
-
-inline void flush() {
-    if (write_buf_pos) {
-        fwrite(write_buf, 1, write_buf_pos, stdout);
-        write_buf_pos = 0;
-        fflush(stdout);
-    }
-}
-
-template <class T>
-inline void writeInt( T x, char end, int output_len ) {
-    if (x < 0) {
-        writeChar('-');
-        x = -x;
-    }
-
-    char s[24];
-    int n = 0;
-    while (x || !n) {
-        s[n++] = '0' + x % 10, x /= 10;
-    }
-    while (n < output_len) {
-        s[n++] = '0';
-    }
-    while (n--) {
-        writeChar(s[n]);
-    }
-    if (end) {
-        writeChar(end);
-    }
-}
-
-inline void writeWord( const char *s ) {
-    while (*s) {
-        writeChar(*s++);
-    }
-}
-
-inline void writeDouble( double x, int output_len ) {
-    if (x < 0) {
-        writeChar('-');
-        x = -x;
-    }
-    assert(x <= (1LLU << 63) - 1);
-    long long t = (long long)x;
-    writeInt(t), x -= t;
-    writeChar('.');
-    for (int i = output_len - 1; i > 0; i--) {
-        x *= 10;
-        t = std::min(9, (int)x);
-        writeChar('0' + t);
-        x -= t;
-    }
-    x *= 10;
-    t = std::min(9, (int)(x + 0.5));
-    writeChar('0' + t);
-}
-
-
-// vim config
-/*
-syntax on
-colorscheme elflord
-
-set showmatch               " show matching 
-set ignorecase              " case insensitive 
-set hlsearch                " highlight search 
-set incsearch               " incremental search
-set tabstop=4               " number of columns occupied by a tab 
-set softtabstop=4           " see multiple spaces as tabstops so <BS> does the right thing
-set shiftwidth=4             " width for autoindents
-"➜⠀·
-set list listchars=tab:➜⠀,space:·
-set expandtab
-
-set number                  " add line numbers
-filetype plugin indent on   "allow auto-indenting depending on file type syntax on" syntax highlighting
-
-set clipboard=unnamed   " using system clipboard
-filetype plugin on
-"set cursorline              " highlight current cursorline
-set ttyfast                 " Speed up scrolling in Vim
-*/
